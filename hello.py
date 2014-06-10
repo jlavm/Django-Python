b015e6b1fb38bbcbb297864d9a84a1905d799e1d
Jorge Lavin Montoro <koke1701@gmail.com>
2014-06-10 11:46:41 +0200

    first function

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print "Hola Mundo"
+
+def fight(fighters,mode="boxing"):
+
+	print "Lucha en modo", mode
+	for fighter in fighters:
+		if(fighter=="Chuck Norris"):
+			print "Ha ganado", fighter
+	print "Chuck no esta"
+
+
+
+fight(["Goku","Bruce Lee","Doraemon"],"kung Fu")
