9691b025b14858fcbf94a2f0a1091494d4a50dbd
Jorge Lavin Montoro <koke1701@gmail.com>
2014-06-12 10:48:04 +0200

    styles added to proyect

--- Frikr/Frikr/Frikr/settings.py
+++ Frikr/Frikr/Frikr/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phots'
+    'phots',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
--- Frikr/Frikr/Frikr/urls.py
+++ Frikr/Frikr/Frikr/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^login$', views.UserLogin.as_view()),
     url(r'^logout$', views.UserLogout.as_view()),
     url(r'^profile$', views.UserProfile.as_view()),
-    url(r'^photos$', views.PhotoListView.as_view()),
+    url(r'^phots$', views.PhotoListView.as_view()),
     url(r'^create$', 'phots.views.create_photo'),
     url(r'^photos/(?P<pk>[0-9]+)$', views.PhotoDetail.as_view())
 )
